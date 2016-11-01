a2f5f85797976cab91aa3943970ee4460c8efc95
ChristofSeiler <christof.seiler@gmail.com>
2016-11-01 10:20:08 -0700

    prepare cluster scripts

---
+++ Installation_R_only.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INSTALL_DIR=$SCRATCH
+
+# install pandoc to knit Rmd
+R_STUDIO=$INSTALL_DIR/rstudio-server-rhel
+mkdir $R_STUDIO
+cd $R_STUDIO
+wget https://download2.rstudio.org/rstudio-server-rhel-0.99.903-x86_64.rpm
+rpm2cpio rstudio-server-rhel-0.99.903-x86_64.rpm | cpio -idmv
+echo "export PATH=${R_STUDIO}/usr/lib/rstudio-server/bin/pandoc:\$PATH" >> ~/.bash_profile
+source ~/.bash_profile
+
+# install R/Bioconductor packages
+module load R/3.3.0
+R -e "source(\"https://bioconductor.org/biocLite.R\"); biocLite(c(\"oligo\",\"limma\",\"hta20sttranscriptcluster.db\",\"BiocParallel\",\"STRINGdb\",\"BioNet\",\"KEGGREST\"),suppressUpdates=TRUE); install.packages(c(\"pheatmap\",\"RColorBrewer\",\"ggplot2\",\"stringr\",\"igraph\",\"visNetwork\",\"rmarkdown\",\"xml2\",\"ggfortify\",\"magrittr\"),repos='http://cran.us.r-project.org')"
---
+++ Makefile
@@ -0,0 +1,17 @@
+# differential analysis
+Oligo.html: Oligo.sbatch Oligo.Rmd
+	sbatch Oligo.sbatch
+
+# install and download resources
+install:
+	bash Installation_R_only.bash
+
+# get cluster update
+check:
+	squeue | grep ${USER}
+
+# clean up
+clean:
+	Oligo.html
+	rm slurm-*.out
+	rm *_results.csv
