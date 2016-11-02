1d1e7dbb6edc778a5b461df421660f1b2da78790
ChristofSeiler <christof.seiler@gmail.com>
2016-11-02 08:30:19 -0700

    remove old files

--- notes.R
+++
@@ -1,9 +0,0 @@
-affyxparser
-
-use arcsinh transform, not log
-
-VST - designed for microarrays
-
-Use STRING: make TSV of network
-Feed network into BioNET
-BioNET - takes skeleton network, finds subnetwork that is most perturbed
