db63f323f91054d9cf9fa4adf5e995248fda76ea
Christof Seiler <christof.seiler@gmail.com>
2020-01-29 21:48:58 +0100

    revisions for manuscript
    exporting ggplot object for combined plot
    exporting gene table

---
+++ Fig2.R
@@ -0,0 +1,22 @@
+# Figure 2
+
+library("ggplot2")
+library("cowplot")
+library("ggthemes")
+theme_set(theme_few())
+scale_colour_discrete = function(...) scale_colour_few()
+
+load("network_t_statistic.Rdata")
+A = network_t_statistic
+load("network_log_coefficient.Rdata")
+B = network_log_coefficient
+load("robust_fit_IFNL_mock.Rdata")
+C = gmine_with_outlier
+load("robust_fit_IFNL_H1N1.Rdata")
+D = gmine_with_outlier
+
+plot_grid(A, C, B, D, nrow = 2, labels = c("A","C","B","D"), 
+          rel_widths = c(3.5, 1), scale = 0.9)
+
+ggsave(filename = "Fig2.jpg", width = 13, height = 8)
+ggsave(filename = "Fig2.tiff", width = 13, height = 8)
